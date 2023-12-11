2ee0466d269675710004232c6e0abb64e8ee6283
¨lizarazu22¨ <lizarazuaramayoignacio@gmail.com>
2023-12-11 19:47:09 -0400

    commit2

--- src/app/(routes)/fase-cierre/page.tsx
+++ src/app/(routes)/fase-cierre/page.tsx
@@ -7,111 +7,172 @@ const FaseCierre = () => {
     <>
       <ContentPage>
         <>
-          <div className="p-4 grid grid-cols-2 gap-4">
+          <div className="p-4 grid grid-cols-3 gap-4">
+            {/* Primer Contrato */}
             <div className="border border-gray-300 p-4 rounded-lg contrato-contenedor">
               <h2 className="text-lg font-semibold mb-2">
-                Contrato de Servicios para el Desarrollo de Sitio Web
+                
+                Manual de Uso del Sitio Web
+
+Acerca del Manual:
+Este manual ofrece una guía paso a paso, tanto gráfica como textual, para utilizar el sitio web eficientemente en dispositivos de escritorio y móviles. Busca garantizar una experiencia de usuario agradable y satisfacer todas las necesidades de información.
+
+Menú del Sitio:
+El sitio web dispone de un menú intuitivo que facilita el acceso a las distintas secciones disponibles.
+
+Uso del Sitio Web en Escritorio:
+1. Eventos: Descripción completa de eventos, con énfasis en sus características principales y acompañados de imágenes.
+2. Cultura: Información sobre talleres, actividades y presentaciones culturales, incluyendo imágenes descriptivas.
+3. Noticias: Las noticias más relevantes para la comunidad académica y el público general.
+4. Políticas: Detalles sobre las reglas, regulaciones y normativas que rigen la institución.
+5. Anuncios: Información sobre cambios administrativos, actualizaciones de políticas, nuevos programas académicos, entre otros, con imágenes relevantes.
+
+Uso del Sitio Web en Móvil:
+- El menú inicial permite explorar las mismas secciones que la versión de escritorio.
+- Las secciones de Eventos, Cultura, Noticias, Políticas y Anuncios mantienen un formato similar al de la versión de escritorio, adaptadas para una navegación móvil eficiente.
+
+Anexos:
+Agradecimiento a los usuarios por su preferencia y compromiso para mantenerlos informados y actualizados.
+
+
               </h2>
               <pre className="contrato-texto">
-                Contrato de Servicios para el Desarrollo de Sitio Web Entre
-                [Nombre de la Empresa de Desarrollo Web] y [Nombre de la
-                Organización de EXPOHOGAR] Fecha: [Fecha de inicio del proyecto]
-                Preámbulo Este contrato se establece entre [Nombre de la Empresa
-                de Desarrollo Web] (en adelante, el Proveedor) y [Nombre de la
-                Organización de EXPOHOGAR] (en adelante, el Cliente), con el
-                propósito de desarrollar y lanzar el sitio web del evento
-                empresarial y de exposición de productos y servicios para el
-                hogar EXPOHOGAR, conforme a los Términos de Referencia
-                proporcionados. El período del proyecto será de tres meses a
-                partir de la fecha de inicio. 1. Alcance del Proyecto El
-                Proveedor se compromete a diseñar y desarrollar un sitio web que
-                incluya: - Diseño y desarrollo según los requerimientos
-                especificados en los Términos de Referencia. - Secciones para
-                noticias, ofertas, información sobre expositores, y formularios
-                de registro. - Implementación de capacidades transaccionales,
-                integración con base de datos, optimización SEO, y diseño
-                responsivo. - Mantenimiento y soporte técnico, incluyendo
-                actualizaciones de seguridad y funcionalidades. 2. Cronograma y
-                Entregables El proyecto se desarrollará durante un período de
-                tres meses, con los siguientes cronograma y entregables: - Fase
-                1: Planificación y desarrollo inicial, entrega [Fecha - un mes
-                después del inicio]. - Fase 2: Desarrollo y pruebas, entrega
-                [Fecha - dos meses después del inicio]. - Fase 3: Lanzamiento y
-                capacitación, entrega [Fecha - tres meses después del inicio]. -
-                Fase 4: Inicio del mantenimiento y soporte, [Fecha - tras el
-                lanzamiento]. 3. Compromisos del Cliente El Cliente se
-                compromete a: - Proveer información y recursos necesarios para
-                el desarrollo del sitio web. - Participar en reuniones de
-                planificación y revisiones. - Aprobar diseños y funcionalidades
-                en las fases establecidas. 4. Términos de Pago Los pagos se
-                estructurarán de la siguiente manera: - 30% tras la aprobación
-                del plan de trabajo. - 40% tras la aprobación de los productos
-                intermedios. - 30% tras la entrega y publicación del sitio web.
-                5. Confidencialidad y Derechos de Autor Toda la información
-                intercambiada durante el proyecto será tratada de manera
-                confidencial. Los derechos de autor del sitio web desarrollado
-                pertenecerán a [Nombre de la Organización de EXPOHOGAR]. 6.
-                Resolución de Disputas Cualquier disputa relacionada con este
-                contrato será resuelta mediante negociación directa o, en caso
-                de no llegar a un acuerdo, mediante arbitraje. Firmas
-                _____________________ [Nombre y firma del representante de la
-                Empresa de Desarrollo Web] _____________________ [Nombre y firma
-                del representante de EXPOHOGAR]
+                {/* Aquí va el contenido del primer contrato */}
               </pre>
             </div>
 
+            {/* Segundo Contrato */}
             <div className="border border-gray-300 p-4 rounded-lg contrato-contenedor">
               <h2 className="text-lg font-semibold mb-2">
-                Contrato de Cierre de Proyecto para el Desarrollo del Sitio Web
-                de EXPOHOGAR
+                Análisis de Métricas de Rendimiento
+
+Metodología:
+El análisis de métricas mide y evalúa el rendimiento del software en desarrollo, enfocándose en tendencias, comportamientos y resultados. Las métricas clave incluyen escalabilidad, estabilidad, capacidad de respuesta, velocidad y disponibilidad.
+
+Métricas Específicas:
+- Escalabilidad: Medida por el número de usuarios simultáneos y el tiempo de respuesta bajo carga.
+- Estabilidad: Evalúa la tasa de error, el tiempo medio entre fallos (MTBF) y el tiempo de recuperación (MTTR).
+- Capacidad de Respuesta: Incluye el tiempo de respuesta del usuario, la latencia de red y la capacidad de procesamiento.
+- Velocidad: Mide el tiempo de carga, renderización y ejecución de consultas.
+- Disponibilidad: Cuantificada a través del tiempo de inactividad y el porcentaje de disponibilidad.
+
+Conclusiones:
+- La escalabilidad actual del sistema es adecuada, pero se requiere monitoreo continuo.
+- La estabilidad necesita mejoras en la tasa de error y MTBF, además de minimizar el MTTR.
+- La capacidad de respuesta puede optimizarse mejorando la infraestructura de red y el procesamiento.
+- Mejorar la velocidad implica optimizar tiempos de carga y renderización, así como la eficiencia de consultas.
+- La disponibilidad se mejora reduciendo el tiempo de inactividad y aumentando el porcentaje de disponibilidad.
+
+Recomendaciones:
+- Realizar pruebas constantes de escalabilidad y adoptar medidas para mejorar la estabilidad.
+- Optimizar la infraestructura de red y procesamiento para mejorar la respuesta.
+- Aplicar técnicas de optimización de código para aumentar la velocidad.
+- Establecer monitoreo constante para mantener alta disponibilidad.
+
+Implementando estas recomendaciones se logrará mejorar significativamente el rendimiento del software, asegurando una experiencia más estable, rápida y accesible para los usuarios.
+
               </h2>
               <pre className="contrato-texto">
-                Contrato de Cierre de Proyecto para el Desarrollo del Sitio Web
-                de EXPOHOGAR Contrato de Cierre de Proyecto para el Desarrollo
-                del Sitio Web de EXPOHOGAR Entre [Nombre de la Empresa de
-                Desarrollo Web] y [Nombre de la Organización de EXPOHOGAR]
-                Fecha: [Fecha - tres meses después del inicio del proyecto]
-                Preámbulo Este contrato de cierre de proyecto reconoce la
-                finalización satisfactoria del desarrollo del sitio web de
-                EXPOHOGAR por parte de [Nombre de la Empresa de Desarrollo Web]
-                (en adelante, el Proveedor) y marca la conclusión del contrato
-                de servicios establecido con [Nombre de la Organización de
-                EXPOHOGAR] (en adelante, el Cliente), tres meses después de su
-                inicio, de acuerdo con los términos y condiciones previamente
-                acordados. 1. Confirmación de la Entrega del Proyecto El
-                Proveedor confirma que, tres meses después del inicio del
-                proyecto, ha completado todos los servicios descritos en el
-                contrato de desarrollo del sitio web, incluyendo: - Desarrollo y
-                diseño del sitio web. - Implementación de funcionalidades según
-                los Términos de Referencia. - Realización de pruebas y ajustes
-                necesarios. - Capacitación al equipo de EXPOHOGAR y entrega de
-                documentación técnica. 2. Aceptación del Cliente El Cliente
-                confirma la recepción y aceptación del sitio web desarrollado,
-                reconociendo que cumple con los requisitos y expectativas
-                establecidos en los Términos de Referencia y en el contrato
-                inicial de desarrollo. 3. Finalización de Pagos El Cliente
-                acuerda realizar el pago final del [porcentaje o monto
-                pendiente] como se estipula en el contrato inicial, completando
-                así la compensación total acordada por los servicios prestados.
-                4. Transferencia de Derechos y Responsabilidades Con la
-                aceptación del sitio web: - Los derechos de propiedad
-                intelectual y autoría del sitio web desarrollado se transfieren
-                al Cliente. - El Cliente asume la responsabilidad total del
-                mantenimiento y gestión del sitio web. - El Proveedor ofrece un
-                período de soporte post-lanzamiento de [duración del soporte],
-                para resolver problemas técnicos menores que puedan surgir. 5.
-                Confidencialidad y No Competencia Ambas partes acuerdan mantener
-                la confidencialidad de la información compartida durante el
-                proyecto y no participar en actividades que puedan considerarse
-                competencia directa o que afecten negativamente a la otra parte,
-                durante un período de [especificar duración]. 6. Cláusulas
-                Finales Este contrato marca la conclusión formal del proyecto de
-                desarrollo del sitio web de EXPOHOGAR tres meses después de su
-                inicio. Cualquier servicio adicional o modificaciones
-                posteriores requerirán un nuevo acuerdo contractual. Firmas
-                _____________________ [Nombre y firma del representante de la
-                Empresa de Desarrollo Web] _____________________ [Nombre y firma
-                del representante de EXPOHOGAR]
+                {/* Aquí va el contenido del segundo contrato */}
+              </pre>
+            </div>
+
+            {/* Tercer Contrato */}
+            <div className="border border-gray-300 p-4 rounded-lg contrato-contenedor">
+              <h2 className="text-lg font-semibold mb-2">
+              CONTRATO DE CIERRE
+
+Entre: La Universidad Privada Boliviana (UPB), en adelante denominada "el Cliente"
+
+Y: [Nombre de la Firma Consultora], en adelante denominada "el Consultor"
+
+1. Objeto del Contrato
+El presente contrato representa el acuerdo de cierre y soporte post-entrega entre el Cliente y el Consultor tras la finalización del proyecto de diseño y desarrollo del portal web "RedLaboralUPB".
+
+2. Período de Soporte Post-Entrega
+El Consultor proporcionará un período de soporte técnico y mantenimiento de tres (3) meses inmediatamente después de la finalización del contrato de desarrollo de un año.
+
+3. Alcance del Soporte Post-Entrega
+Durante el período de soporte post-entrega, el Consultor se compromete a:
+   - Corregir errores y problemas técnicos que se identifiquen después de la entrega final del portal.
+   - Brindar asistencia técnica y soporte a los usuarios finales y al personal técnico de la UPB.
+   - Implementar pequeñas mejoras y actualizaciones que no constituyan un desarrollo mayor.
+
+4. Duración del Soporte Post-Entrega
+El soporte post-entrega comenzará el [Fecha de inicio] y concluirá el [Fecha de fin], tres meses después.
+
+5. Compensación
+Por los servicios de soporte post-entrega, el Cliente acordará una compensación que se detallará en este contrato y que será pagada en su totalidad al finalizar el período de tres meses, a menos que se acuerde de otra manera.
+
+6. Obligaciones del Consultor
+El Consultor se compromete a:
+   - Responder a las solicitudes de soporte dentro de un plazo razonable acordado por ambas partes.
+   - Mantener la funcionalidad y rendimiento del portal conforme a los estándares acordados.
+
+7. Obligaciones del Cliente
+El Cliente se compromete a:
+   - Notificar al Consultor sobre cualquier problema o requerimiento de soporte de manera oportuna.
+   - Proveer cualquier información necesaria para la resolución de incidencias.
+
+8. Detalle de Costos del Proyecto
+A continuación se detalla la estructura de costos asociados a los diferentes módulos desarrollados durante el proyecto:
+
+   - Módulo 1: Gestión de Usuarios
+     - Costo: [Costo estimado del Módulo 1]
+   - Módulo 2: Vistas de Información
+     - Costo: [Costo estimado del Módulo 2]
+   - Módulo 3: Procesos de Selección de Personal
+     - Costo: [Costo estimado del Módulo 3]
+   - Módulo 4: Comunicación Interna
+     - Costo: [Costo estimado del Módulo 4]
+   - Módulo 5: Desarrollo de Contenidos
+     - Costo: [Costo estimado del Módulo 5]
+
+9. Outline de Entregables Concluidos
+Al término del proyecto, el Consultor entregó lo siguiente:
+
+   a. Portal web "RedLaboralUPB" completamente funcional.
+   b. Sistema de gestión de conocimientos integrado.
+   c. Documentación técnica y manuales de usuario.
+   d. Informes de pruebas de usabilidad y rendimiento.
+   e. Servicio de hosting y protocolos de seguridad implementados.
+   f. Sesiones de capacitación para el personal de la UPB.
+
+10. Cierre del Proyecto
+Al término del período de soporte post-entrega, se llevará a cabo una reunión de cierre de proyecto para confirmar que todas las obligaciones contractuales se han cumplido satisfactoriamente.
+
+11. Propiedad Intelectual
+La propiedad intelectual de los entregables desarrollados durante el contrato de desarrollo de un año seguirá siendo propiedad del Consultor a menos que se especifique lo contrario.
+
+12. Cláusula de Confidencialidad
+Las obligaciones de confidencialidad continuarán vigentes incluso después del cierre del presente contrato según lo estipulado en el contrato original de servicios de consultoría.
+
+13. Resolución de Conflictos
+Las partes acuerdan que cualquier disputa surgida en relación con este contrato será resuelta mediante arbitraje, a menos que se resuelva amistosamente entre las partes.
+
+14. Firma de las Partes
+Este contrato es un complemento al contrato original y no invalida ninguna de
+
+ las cláusulas previamente acordadas a menos que se especifique expresamente en este documento.
+
+Firmado:
+
+Por el Cliente: _____________________
+Nombre:
+Cargo:
+Fecha:
+
+Por el Consultor: ___________________
+Nombre:
+Cargo:
+Fecha:
+
+
+ 
+
+              </h2>
+              <pre className="contrato-texto">
+                {/* Aquí va el contenido del tercer contrato */}
               </pre>
             </div>
           </div>
@@ -123,46 +184,39 @@ const FaseCierre = () => {
             <li>
               <a
                 className="text-blue-400 hover:font-semibold hover:text-blue-800"
-                href="https://docs.google.com/document/d/1jr_wlWRF3MtDzb8rCBY_plVACNt3MOAUccaTX8BVlIA/edit?usp=sharing"
+                href="https://docs.google.com/document/d/1clfb4rh8dLyBKP6P1WFczkzwyR4UlCPerTXS-z6P3x8/edit"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Carte de entrega de producto
+                Manual de Usuario
               </a>
             </li>
             <li>
               <a
                 className="text-blue-400 hover:font-semibold hover:text-blue-800"
-                href="https://docs.google.com/document/d/1ZPFNDe69_Zg6aZ_0QV9wVoFK9_ihAeulmqtmknNdtEE/edit?usp=drive_link"
+                href="https://docs.google.com/document/d/1w4_9UlqFbHP2DEA3dhBRkWddiNRH499c4R4kJirliLM/edit"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Manual del Sistema
+                Análisis de Métricas de Rendimiento
               </a>
             </li>
             <li>
               <a
                 className="text-blue-400 hover:font-semibold hover:text-blue-800"
-                href="https://docs.google.com/document/d/1Gy0KaWyN0XzjQJYTXku_hSs3Ul-s7s4H5zgzRz9VwSQ/edit?usp=drive_link"
+                href="https://docs.google.com/document/d/1P7YjJzcUr7dGsoxH4hSmKT1p6kA0gBsMczXHTn-4EDs/edit"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Manual de uso - Expositores
+                Contrato de cierre 
               </a>
             </li>
-            <li>
-              <a
-                className="text-blue-400 hover:font-semibold hover:text-blue-800"
-                href="https://docs.google.com/document/d/1wNMtgPZLKmVCpXdR8XT3B5xh7lypAbRKN7lYtasS_2k/edit?usp=drive_link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Manual de uso - Usuarios
-              </a>
-            </li>
+            
           </ul>
         </>
       </ContentPage>
+
+      {/* Estilos */}
       <style jsx>
         {`
           .contrato-contenedor {
